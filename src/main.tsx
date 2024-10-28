e6eec75619748577a5da54a3998bdda3c004ce48
Zebedee Lewis <zebedeeslewis@gmail.com>
2024-10-28 16:00:23 -0600

    feat: setup routing with TanStackRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import App from './app/app';// Import the generated route tree
 
-import App from './app/app';
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
