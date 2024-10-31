4c355c2b59c0ef71f6707c660465da8027f8d269
Zebedee Lewis <zebedeeslewis@gmail.com>
2024-10-31 10:26:23 -0600

    feat: setup navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import { Link } from '@tanstack/react-router'
 import './navbar.less'
 import logo from '../../assets/logo.svg'
@@ -5,15 +6,18 @@ import hamburger from '../../assets/hamburger.svg'
 import close from '../../assets/close.svg'
 
 export default function Navbar() {
+  const [toggled, setToggled] = useState(false)
+  const toggle = ()=>setToggled(!toggled)
+
   return (
-    <div className="navbar toggled">
-      <nav className="navbar-header primary-container">
+    <div className={`navbar navbar-${toggled?'toggled':''}`}>
+      <nav className="navbar-header">
         <Link to="/">
           <div className="image-wrapper navbar-logo">
             <img alt="logo" loading="lazy" src={logo} />
           </div>
         </Link>
-        <button className="navbar-toggle">
+        <button className="navbar-toggle" onClick={toggle} >
           <div className="image-wrapper">
             <img className="hamburger"
               alt="navbar-hamburger"
@@ -28,11 +32,13 @@ export default function Navbar() {
           </div>
         </button>
       </nav>
-      <nav className="navbar-links primary-container">
-        <Link to="/about" className="nav-link">Our Company</Link>
-        <Link to="/location" className="nav-link">Locations</Link>
-        <Link to="/contact" className="nav-link">Contact</Link>
-      </nav>
+      <div className="navbar-body">
+        <nav className="navbar-body-inner">
+          <Link to="/about" className="nav-link">Our Company</Link>
+          <Link to="/location" className="nav-link">Locations</Link>
+          <Link to="/contact" className="nav-link">Contact</Link>
+        </nav>
+      </div>
     </div>
   )
 }
