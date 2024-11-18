8c065fb80f4481e27858991c12c177ff714a5598
Zebedee Lewis <zebedeeslewis@gmail.com>
2024-11-17 22:06:19 -0600

    feat(components/card): create card component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,18 +1,30 @@
-import {FunctionComponent, useRef} from 'react'
+import {FunctionComponent, CSSProperties} from 'react'
+import './card.less'
+
+interface BackgroundVariableCss extends CSSProperties {
+  '--background-image'?: string;
+  '--background-color'?: string;
+}
 
 export interface CardContent
   { heading: string
   , image: string
   , summary: string
+  , backgroundColor: string
   }
 
 export const Card:FunctionComponent<{content:CardContent}>
-= ({content:{heading, image, summary}}) => {
-  const cardRef = useRef(null)
+= ({content:{heading, image, summary, backgroundColor}}) => {
+  const backgroundVariableCss: BackgroundVariableCss
+    = { '--background-image': `url(${image})`
+      , '--background-color': backgroundColor
+      , }
 
-  return <div ref={cardRef} className="portfolioCard">
-    <h2 className="portfolioCard-heading">{heading}</h2>
-    {image}
-    <p className="portfolioCard-summary">{summary}</p>
+
+  return <div style={backgroundVariableCss} className="portfolioCard-variableContainer">
+    <div className="portfolioCard">
+      <h2 className="portfolioCard-heading">{heading}</h2>
+      <p className="portfolioCard-summary">{summary}</p>
+    </div>
   </div>
 }
