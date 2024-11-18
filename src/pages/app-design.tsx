c9dfc94f27e199fec69f0589a8cd8881e0898623
Zebedee Lewis <zebedeeslewis@gmail.com>
2024-11-17 22:07:33 -0600

    WIP

--- src/pages/app-design.tsx
+++ src/pages/app-design.tsx
@@ -3,11 +3,11 @@ import { ServicePageContent, PageContent } from './_service-page-content'
 
 import './app-design.less'
 
-import loopstudiosHeaderImage from '../assets/loopstudio-header-image.jpg'
-import todoHeaderImage from '../assets/todo-header-image.jpg'
-import faceitHeaderImage from '../assets/faceit-header-image.jpg'
-import eyecamHeaderImage from '../assets/eyecam-header-image.jpg'
-import airfilterHeaderImage from '../assets/airfilter-header-image.jpg'
+import loopstudiosHeaderImage from '../assets/loopstudio-header-image.png'
+import todoHeaderImage from '../assets/todo-header-image.png'
+import faceitHeaderImage from '../assets/faceit-header-image.png'
+import eyecamHeaderImage from '../assets/eyecam-header-image.png'
+import airfilterHeaderImage from '../assets/airfilter-header-image.png'
 
 const content: PageContent =
   { heading: "app design"
@@ -15,27 +15,27 @@ const content: PageContent =
   , portfolio:
     [ { heading: "airfilter"
       , image: airfilterHeaderImage
-      , backgroundColor: "red"
+      , backgroundColor: "#2E3947"
       , summary: "solving the problem of poor indoor air quality by filtering the air."
       } 
     , { heading: "eyecam"
       , image: eyecamHeaderImage
-      , backgroundColor: "red"
+      , backgroundColor: "#34B7A2"
       , summary: "product that lets you edit your favorite photos and videos at any time"
       }
     , { heading: "faceit"
       , image: faceitHeaderImage
-      , backgroundColor: "red"
+      , backgroundColor: "#3F2F83"
       , summary: "get to meet your favorite internet superstar with the faceit app"
       }
     , { heading: "todo"
       , image: todoHeaderImage
-      , backgroundColor: "red"
+      , backgroundColor: "#40125A"
       , summary: "a todo app that features cloud sync with light and dark mode"
       }
     , { heading: "loopstudios"
       , image: loopstudiosHeaderImage
-      , backgroundColor: "red"
+      , backgroundColor: "#853A4D"
       , summary: "a VR experience app made for loopstudios"
       }
     ]
