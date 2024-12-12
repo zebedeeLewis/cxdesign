94de5ab402039a260f4e1a1832bf86df59b34576
Zebedee Lewis <zebedeeslewis@gmail.com>
2024-12-11 18:08:25 -0600

    build

---
+++ dist/designio/assets/location.lazy-DzrrXkks.js
@@ -0,0 +1,1 @@
+import{j as o,c as t}from"./index-C3T4cSFs.js";import{L as a}from"./primary-BcWKPdYe.js";const e=()=>o.jsx(a,{pageName:"location-page",children:"Hello from Location Page!"}),i=t("/location")({component:e});export{i as Route};
